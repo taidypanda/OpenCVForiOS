8c398c2ecdee1b25c388b3c09c96118d85f1f689
taidy <xiongguob@163.com>
2016-11-18 16:55:56 +0800

    Initial Commit

---
+++ PanoramaPhoto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PanoramaPhoto
+//
+//  Created by 苹果 on 2016/11/18.
+//  Copyright © 2016年 苹果. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PanoramaPhoto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PanoramaPhoto
+//
+//  Created by 苹果 on 2016/11/18.
+//  Copyright © 2016年 苹果. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
